165b18dadb497c24811c096dd504684d16071e7b
gdghds0 <66929407+gdghds0@users.noreply.github.com>
2023-09-25 19:09:57 +0800

    Delete FSRENet-master directory

--- FSRENet-master/train_FSREnet_2d.sh
+++
@@ -1,5 +0,0 @@
-python ./code/FSRENet_ACDC.py  --model FSRENet_ACDC --labelnum 3 &&
-python ./code/FSRENet_ACDC.py  --model FSRENet_ACDC --labelnum 7 &&
-python ./code/FSRENet_ACDC.py  --model FSRENet_ACDC --labelnum 14 &&
-python ./code/FSRENet_ACDC.py  --model FSRENet_ACDC --labelnum 35 &&
-
--- FSRENet-master/train_FSREnet_3d.sh
+++
@@ -1,3 +0,0 @@
-python ./code/FSRENet_LA.py  --model FSRENet_LA --labelnum 4 &&
-python ./code/FSRENet_LA.py  --model FSRENet_LA --labelnum 8 
-
